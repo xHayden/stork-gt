e701adabb4cdb06a666c2d069d91422067d6ca5e
Hayden Carpenter <haydencarp@outlook.com>
2023-07-23 15:22:02 -0400

    fix eslint apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex justify-center items-center flex-grow">
       <div className='w-11/12 md:w-2/3 gap-8 flex flex-col'> 
-        <h1 className="text-5xl md:text-7xl lg:text-8xl text-yellow-200">It's storkin' time!</h1>
+        <h1 className="text-5xl md:text-7xl lg:text-8xl text-yellow-200">It&apos;s storkin&apos; time!</h1>
         <p className='text-white text-xl md:text-3xl'>
           Get ready for the only Fantasy League where teams bet on
           which storks will migrate the fastest.
